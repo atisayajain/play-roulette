c985c6b54d3dd1d0981bdef72bd1e9d55858f56f
atisayajain <atisayajain@gmail.com>
2021-11-10 04:01:04 +0530

    add playercasinomap to admin

--- players/admin.py
+++ players/admin.py
@@ -6,5 +6,9 @@ from . import models
 class PlayerAdmin(admin.ModelAdmin):
     pass
 
+class PlayerCasinoAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(models.Player, PlayerAdmin)
+admin.site.register(models.PlayerCasinoMap, PlayerCasinoAdmin)
